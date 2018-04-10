0bbfa21fce2c6d1627cab57ff2fe0cee86606fd2
k.sakimura <sakikazu15@gmail.com>
2018-04-10 18:21:25 +0900

    Handの判定を実装。PairHandのみ、テストが通ってない

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -35,6 +35,42 @@ describe Hand do
   end
 
   describe PairHand do
+    describe 'compare_cards' do
+      before do
+        card1 = Card.new('(h)', 7)
+        card2 = Card.new('(h)', 7)
+        card3 = Card.new('(h)', 1)
+        @hand1 = PairHand.new([card1, card2, card3])
+
+        card4 = Card.new('(h)', 1)
+        card5 = Card.new('(h)', 7)
+        card6 = Card.new('(h)', 1)
+        @hand2 = PairHand.new([card4, card5, card6])
+      end
+
+      it 'should be 1' do
+        expect(@hand1.compare(@hand2)).to eq 1
+      end
+
+      context 'same rank' do
+        before do
+          card1 = Card.new('(h)', 7)
+          card2 = Card.new('(h)', 7)
+          card3 = Card.new('(h)', 1)
+          @hand1 = PairHand.new([card1, card2, card3])
+
+          card4 = Card.new('(h)', 7)
+          card5 = Card.new('(h)', 13)
+          card6 = Card.new('(h)', 7)
+          @hand2 = PairHand.new([card4, card5, card6])
+        end
+
+        it 'should be -1' do
+          expect(@hand1.compare(@hand2)).to eq -1
+        end
+      end
+    end
+
     describe 'match_cards_notation' do
       before do
         card1 = Card.new('(h)', 1)
@@ -45,6 +81,58 @@ describe Hand do
 
       it 'should be A(h)|A(s)' do
         expect(@hand.match_cards_notation).to eq 'A(h)|A(s)'
+      end
+    end
+  end
+
+  describe StraightFlushHand do
+    describe 'match_cards_notation' do
+      context '2(h), 3(h), A(h)' do
+        before do
+          card1 = Card.new('(h)', 2)
+          card2 = Card.new('(h)', 3)
+          card3 = Card.new('(h)', 1)
+          @hand = StraightFlushHand.new([card1, card2, card3])
+        end
+
+        it 'should be A(h)|2(h)|3(h)' do
+          expect(@hand.match_cards_notation).to eq 'A(h)|2(h)|3(h)'
+        end
+      end
+
+      context 'K(h), A(h), Q(h)' do
+        before do
+          card1 = Card.new('(h)', 13)
+          card2 = Card.new('(h)', 1)
+          card3 = Card.new('(h)', 12)
+          @hand = StraightFlushHand.new([card1, card2, card3])
+        end
+
+        it 'should be Q(h)|K(h)|A(h)' do
+          expect(@hand.match_cards_notation).to eq 'Q(h)|K(h)|A(h)'
+        end
+      end
+    end
+
+    describe 'compare_cards' do
+      context 'save hand' do
+        context '[2(h), 3(h), A(h)] AND [K(h), A(h), Q(h)]' do
+          before do
+            card1 = Card.new('(h)', 2)
+            card2 = Card.new('(h)', 3)
+            card3 = Card.new('(h)', 1)
+            @hand1 = StraightFlushHand.new([card1, card2, card3])
+
+            card4 = Card.new('(h)', 13)
+            card5 = Card.new('(h)', 1)
+            card6 = Card.new('(h)', 12)
+            @hand2 = StraightFlushHand.new([card4, card5, card6])
+          end
+
+          it 'should be 1' do
+            expect(@hand1.compare(@hand2)).to eq 1
+          end
+        end
       end
     end
   end
--- src/hand.rb
+++ src/hand.rb
@@ -13,6 +13,7 @@ class Hand
     raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
   end
 
+  # TODO: HandCheckerでやるべきだと思う。その際に、ストレートの強さの仕様など含めて、強い順で取得できるように
   def match_cards
     raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
   end
@@ -35,6 +36,16 @@ class Hand
   def compare_cards(opponent_hand)
     raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
   end
+
+  def self.compare_point(card1, card2)
+    if card1.point_index > card2.point_index
+      return -1
+    elsif card2.point_index > card1.point_index
+      return 1
+    else
+      0
+    end
+  end
 end
 
 class HighCardHand < Hand
@@ -51,20 +62,26 @@ class HighCardHand < Hand
   end
 
   def compare_cards(opponent_hand)
-    compare_recursive(match_cards, opponent_hand.match_cards, match_cards.size-1)
+    self_cards = match_cards
+    opponent_cards = opponent_hand.match_cards
+    (self_cards.size-1).downto 0 do |n|
+      result = Hand.compare_point(self_cards[n], opponent_cards[n])
+      return result if result != 0
+    end
+    0
   end
 
-  # TODO loopでいけるんじゃ？
-  def compare_recursive(cards, opponent_cards, n)
-    return 0 if n < 0
-    if cards[n].point_index > opponent_cards[n].point_index
-      -1
-    elsif opponent_cards[n].point_index > cards[n].point_index
-      1
-    else
-      compare_recursive(cards, opponent_cards, n-1)
-    end
-  end
+  # NOTE: 再帰でやる必要なかった
+  # def compare_recursive(cards, opponent_cards, n)
+    # return 0 if n < 0
+    # if cards[n].point_index > opponent_cards[n].point_index
+      # -1
+    # elsif opponent_cards[n].point_index > cards[n].point_index
+      # 1
+    # else
+      # compare_recursive(cards, opponent_cards, n-1)
+    # end
+  # end
 end
 
 class PairHand < Hand
@@ -81,9 +98,11 @@ class PairHand < Hand
     found_pair_cards[1].sort_by { |card| card.point_index }
   end
 
-  require 'byebug'
-  # todo ペアのランクが同じなら、もう一つのカードで判定するのか・・matchとotherとを分ける必要がある
+  # NOTE: ペアのランクが同じなら、もう一つのカードで判定する
   def compare_cards(opponent_hand)
+    strongest_card = match_cards.last
+    opponent_strongest_card = opponent_hand.match_cards.last
+    Hand.compare_point(strongest_card, opponent_strongest_card)
   end
 end
 
@@ -101,7 +120,13 @@ class FlushHand < Hand
   end
 
   def compare_cards(opponent_hand)
-    # todo HighCardHandと同じ
+    self_cards = match_cards
+    opponent_cards = opponent_hand.match_cards
+    (self_cards.size-1).downto 0 do |n|
+      result = Hand.compare_point(self_cards[n], opponent_cards[n])
+      return result if result != 0
+    end
+    0
   end
 end
 
@@ -115,12 +140,18 @@ class StraightHand < Hand
   end
 
   def match_cards
+    cards_ordered_number = @cards.sort_by { |card| card.number_index }
+    if cards_ordered_number.map(&:rank) == ["A", "2", "3"]
+      return cards_ordered_number
+    end
     @cards.sort_by { |card| card.point_index }
   end
 
   def compare_cards(opponent_hand)
+    strongest_card = match_cards.last
+    opponent_strongest_card = opponent_hand.match_cards.last
+    Hand.compare_point(strongest_card, opponent_strongest_card)
   end
-
 end
 
 class ThreeOfAKindHand < Hand
@@ -138,6 +169,9 @@ class ThreeOfAKindHand < Hand
   end
 
   def compare_cards(opponent_hand)
+    strongest_card = match_cards.last
+    opponent_strongest_card = opponent_hand.match_cards.last
+    Hand.compare_point(strongest_card, opponent_strongest_card)
   end
 end
 
@@ -151,9 +185,16 @@ class StraightFlushHand < Hand
   end
 
   def match_cards
+    cards_ordered_number = @cards.sort_by { |card| card.number_index }
+    if cards_ordered_number.map(&:rank) == ["A", "2", "3"]
+      return cards_ordered_number
+    end
     @cards.sort_by { |card| card.point_index }
   end
 
   def compare_cards(opponent_hand)
+    strongest_card = match_cards.last
+    opponent_strongest_card = opponent_hand.match_cards.last
+    Hand.compare_point(strongest_card, opponent_strongest_card)
   end
 end
